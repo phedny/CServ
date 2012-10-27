28ea91665691f4625690ee6c7867c67660ec4ab2
Sjors Gielen <sjors@sjorsgielen.nl>
2012-10-27 14:52:41 +0200

    Adds getActivatedSimsWithoutActivationInvoice() and getActivatedSimsLastInvoicedBefore() to SimService.

--- nl.limesco.cserv.sim/src/nl/limesco/cserv/sim/api/SimService.java
+++ nl.limesco.cserv.sim/src/nl/limesco/cserv/sim/api/SimService.java
@@ -1,5 +1,6 @@
 package nl.limesco.cserv.sim.api;
 
+import java.util.Calendar;
 import java.util.Collection;
 
 import com.google.common.base.Optional;
@@ -7,6 +8,13 @@ import com.google.common.base.Optional;
 public interface SimService {
 	Optional<? extends Sim> getSimByIccid(String iccid);
 	Collection<? extends Sim> getSimsByOwnerAccountId(String accountId);
+	Collection<? extends Sim> getActivatedSimsWithoutActivationInvoice();
+	/**
+	 * @param month Date object, of which only year and month are used.
+	 * @return a collection of sims whose monthly fees were last invoiced before the
+	 * month in the given parameter.
+	 */
+	Collection<? extends Sim> getActivatedSimsLastInvoicedBefore(Calendar month);
 	
 	Sim registerSim(String iccid, String puk);
 	void updateSim(Sim sim);
--- nl.limesco.cserv.sim/src/nl/limesco/cserv/sim/mongo/SimServiceImpl.java
+++ nl.limesco.cserv.sim/src/nl/limesco/cserv/sim/mongo/SimServiceImpl.java
@@ -3,19 +3,23 @@ package nl.limesco.cserv.sim.mongo;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Iterator;
 
 import net.vz.mongodb.jackson.DBCursor;
+import net.vz.mongodb.jackson.DBQuery;
 import net.vz.mongodb.jackson.JacksonDBCollection;
 import nl.limesco.cserv.sim.api.Sim;
 import nl.limesco.cserv.sim.api.SimService;
+import nl.limesco.cserv.sim.api.SimState;
 
 import org.amdatu.mongo.MongoDBService;
 import org.bson.types.ObjectId;
 
 import com.google.common.base.Optional;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
 
@@ -37,7 +41,30 @@ public class SimServiceImpl implements SimService {
 		final DBCursor<SimImpl> invoiceCursor = collection().find(new BasicDBObject().append("owner", accountId));
 		return Lists.newArrayList((Iterator<SimImpl>) invoiceCursor);
 	}
+	
+	@Override
+	public Collection<? extends Sim> getActivatedSimsWithoutActivationInvoice() {
+		final DBCursor<SimImpl> invoiceCursor = collection().find(DBQuery.and(
+				DBQuery.in("state", SimState.ACTIVATED, SimState.ACTIVATION_REQUESTED),
+				DBQuery.notExists("activationInvoiceId")));
+		return Sets.newHashSet((Iterator<SimImpl>) invoiceCursor);
+	}
 
+	@Override
+	public Collection<? extends Sim> getActivatedSimsLastInvoicedBefore(Calendar monthCal) {
+		int year = monthCal.get(Calendar.YEAR);
+		int month = monthCal.get(Calendar.MONTH);
+		final DBCursor<SimImpl> invoiceCursor = collection().find(DBQuery.or(
+				DBQuery.notExists("lastMonthlyFeesInvoice"),
+				DBQuery.lessThan("lastMonthlyFeesInvoice.year", year),
+				DBQuery.and(
+						DBQuery.lessThanEquals("lastMonthlyFeesInvoice.year", year),
+						DBQuery.lessThan("lastMonthlyFeesInvoice.month", month)
+				)
+			));
+		return Sets.newHashSet((Iterator<SimImpl>) invoiceCursor);
+	}
+	
 	@Override
 	public Optional<? extends Sim> getSimByIccid(String iccid) {
 		checkNotNull(iccid);
