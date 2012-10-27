46cbd5fe3e91e9b7f22d427a7b884a3e08e333a2
Sjors Gielen <sjors@sjorsgielen.nl>
2012-10-27 22:34:08 +0200

    Added a REST API to the nl.limesco.cserv.sim project to administer SIMs.
    * Adds GET to /sims/{simId}
    * Adds GET and POST to /sims

---
+++ nl.limesco.cserv.sim/src/nl/limesco/cserv/sim/rest/Activator.java
@@ -0,0 +1,30 @@
+package nl.limesco.cserv.sim.rest;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response.Status;
+
+import nl.limesco.cserv.account.api.AccountService;
+import nl.limesco.cserv.auth.api.WebAuthorizationService;
+import nl.limesco.cserv.sim.api.SimService;
+import nl.limesco.cserv.util.dm.UnavailableOSGiService;
+
+import org.apache.felix.dm.DependencyActivatorBase;
+import org.apache.felix.dm.DependencyManager;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends DependencyActivatorBase {
+
+	@Override
+	public void init(BundleContext context, DependencyManager manager) throws Exception {
+		manager.add(createComponent()
+				.setInterface(Object.class.getName(), null)
+				.setImplementation(SimResource.class)
+				.add(createServiceDependency().setService(WebAuthorizationService.class).setRequired(true))
+				.add(createServiceDependency().setService(SimService.class).setRequired(true).setDefaultImplementation(UnavailableOSGiService.newInstance(SimService.class, WebApplicationException.class, Status.SERVICE_UNAVAILABLE))));
+	}
+
+	@Override
+	public void destroy(BundleContext context, DependencyManager manager) throws Exception {
+	}
+
+}
---
+++ nl.limesco.cserv.sim/src/nl/limesco/cserv/sim/rest/SimResource.java
@@ -0,0 +1,113 @@
+package nl.limesco.cserv.sim.rest;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import nl.limesco.cserv.auth.api.Role;
+import nl.limesco.cserv.auth.api.WebAuthorizationService;
+import nl.limesco.cserv.sim.api.Sim;
+import nl.limesco.cserv.sim.api.SimService;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.google.common.base.Optional;
+
+@Path("sims")
+public class SimResource {
+	
+	private volatile WebAuthorizationService authorizationService;
+	
+	private volatile SimService simService;
+
+	@Path("{simId}")
+	public SimSubResource getSim(@PathParam("simId") String id, @Context HttpServletRequest request) {
+		String accountId = authorizationService.requiredAccountId(request);
+		final Optional<? extends Sim> optSim = simService.getSimByIccid(id);
+		if(!optSim.isPresent()) {
+			// Give 403 if the requester is not an admin, 404 otherwise
+			// (as to not too easily expose whether SIMs exist to non-admins)
+			authorizationService.requireUserRole(request, Role.ADMIN);
+			throw new WebApplicationException(Status.NOT_FOUND);
+		}
+		final Sim sim = optSim.get();
+		if(!sim.getOwnerAccountId().equals(accountId)) {
+			authorizationService.requireUserRole(request, Role.ADMIN);
+		}
+		return new SimSubResource(sim);
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createNewSim(String json, @Context HttpServletRequest request) {
+		authorizationService.requireUserRole(request, Role.ADMIN);
+		
+		try {
+			final Sim newSim = simService.createSimFromJson(json);
+			if(simService.getSimByIccid(newSim.getIccid()).isPresent()) {
+				throw new WebApplicationException(Status.CONFLICT);
+			}
+			simService.storeSim(newSim);
+			return Response.created(new URI(newSim.getIccid())).build();
+		} catch (IOException e) {
+			throw new WebApplicationException(e);
+		} catch (URISyntaxException e) {
+			throw new WebApplicationException(e);
+		}
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getMySims(String json, @Context HttpServletRequest request) {
+		String accountId = authorizationService.requiredAccountId(request);
+		Collection<? extends Sim> sims = simService.getSimsByOwnerAccountId(accountId);
+		try {
+			return new ObjectMapper().writeValueAsString(sims);
+		} catch(JsonGenerationException e) {
+			throw new WebApplicationException(e);
+		} catch(JsonMappingException e) {
+			throw new WebApplicationException(e);
+		} catch(IOException e) {
+			throw new WebApplicationException(e);
+		}
+	}
+	
+	public class SimSubResource {
+		private final Sim sim;
+		
+		public SimSubResource(Sim sim) {
+			this.sim = sim;
+		}
+	
+		@GET
+		@Produces(MediaType.APPLICATION_JSON)
+		public String getSim() {
+			try {
+				return new ObjectMapper().writeValueAsString(this.sim);
+			} catch(JsonGenerationException e) {
+				throw new WebApplicationException(e);
+			} catch(JsonMappingException e) {
+				throw new WebApplicationException(e);
+			} catch(IOException e) {
+				throw new WebApplicationException(e);
+			}
+		}
+	}
+
+}
