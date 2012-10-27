6f5ddaef1777fbd4cb980bf311fe5cb43cd9baf7
Sjors Gielen <sjors@sjorsgielen.nl>
2012-10-27 22:22:25 +0200

    Don't try to restore or save the fullName to JSON, it is just a handy tool

--- nl.limesco.cserv.account/src/nl/limesco/cserv/account/api/Name.java
+++ nl.limesco.cserv.account/src/nl/limesco/cserv/account/api/Name.java
@@ -1,10 +1,13 @@
 package nl.limesco.cserv.account.api;
 
+import org.codehaus.jackson.annotate.JsonIgnore;
+
 public class Name {
 	private String firstName;
 	/// Last name, including surname prefix (tussenvoegsel)
 	private String lastName;
 	
+	@JsonIgnore
 	public String getFullName() {
 		return firstName + " " + lastName;
 	}
