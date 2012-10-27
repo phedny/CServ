d9dbfcfd83ef03804f8b8ee06b02f239c51b582a
Sjors Gielen <sjors@sjorsgielen.nl>
2012-10-27 16:56:05 +0200

    sipUri -> uri, remove hostname

--- nl.limesco.cserv.sim/src/nl/limesco/cserv/sim/api/SipSettings.java
+++ nl.limesco.cserv.sim/src/nl/limesco/cserv/sim/api/SipSettings.java
@@ -1,20 +1,13 @@
 package nl.limesco.cserv.sim.api;
 
 public class SipSettings {
-	private String hostname;
 	private String realm;
 	private String username;
 	private String authenticationUsername;
 	private String password;
-	private String sipUri;
+	private String uri;
 	private int expiry;
 	
-	public String getHostname() {
-		return hostname;
-	}
-	public void setHostname(String hostname) {
-		this.hostname = hostname;
-	}
 	public String getRealm() {
 		return realm;
 	}
@@ -39,11 +32,11 @@ public class SipSettings {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public String getSipUri() {
-		return sipUri;
+	public String getUri() {
+		return uri;
 	}
-	public void setSipUri(String sipUri) {
-		this.sipUri = sipUri;
+	public void setUri(String uri) {
+		this.uri = uri;
 	}
 	public int getExpiry() {
 		return expiry;
