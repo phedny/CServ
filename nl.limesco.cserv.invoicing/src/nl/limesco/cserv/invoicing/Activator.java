a3a155b31e7379169ff1b54ca60b62032a6dbd2a
Mark van Cuijk <github@mark.van-cuijk.nl>
2013-01-13 17:32:39 +0100

    Add code for mailing invoices

--- nl.limesco.cserv.invoicing/src/nl/limesco/cserv/invoicing/Activator.java
+++ nl.limesco.cserv.invoicing/src/nl/limesco/cserv/invoicing/Activator.java
@@ -5,6 +5,7 @@ import nl.limesco.cserv.cdr.api.CdrService;
 import nl.limesco.cserv.invoice.api.BatchInvoicingService;
 import nl.limesco.cserv.invoice.api.InvoiceService;
 import nl.limesco.cserv.invoice.api.InvoiceTransformationService;
+import nl.limesco.cserv.lib.email.api.EmailService;
 import nl.limesco.cserv.pricing.api.PricingService;
 import nl.limesco.cserv.sim.api.SimService;
 
@@ -21,6 +22,7 @@ public class Activator extends DependencyActivatorBase {
 				.setInterface(BatchInvoicingService.class.getName(), null)
 				.setImplementation(BatchInvoicingServiceImpl.class)
 				.setComposition("getComposition")
+				.add(createServiceDependency().setService(EmailService.class).setRequired(true))
 				.add(createServiceDependency().setService(InvoiceService.class).setRequired(true))
 				.add(createServiceDependency().setService(AccountService.class).setRequired(true))
 				.add(createServiceDependency().setService(PricingService.class).setRequired(true))
--- nl.limesco.cserv.invoicing/src/nl/limesco/cserv/invoicing/BatchInvoicingServiceImpl.java
+++ nl.limesco.cserv.invoicing/src/nl/limesco/cserv/invoicing/BatchInvoicingServiceImpl.java
@@ -2,6 +2,8 @@ package nl.limesco.cserv.invoicing;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Set;
@@ -18,6 +20,7 @@ import nl.limesco.cserv.invoice.api.IdAllocationException;
 import nl.limesco.cserv.invoice.api.Invoice;
 import nl.limesco.cserv.invoice.api.InvoiceService;
 import nl.limesco.cserv.invoice.api.InvoiceTransformationService;
+import nl.limesco.cserv.lib.email.api.EmailService;
 import nl.limesco.cserv.pricing.api.DataPricingRule;
 import nl.limesco.cserv.pricing.api.NoApplicablePricingRuleException;
 import nl.limesco.cserv.pricing.api.PricingRuleNotApplicableException;
@@ -28,15 +31,18 @@ import nl.limesco.cserv.sim.api.CallConnectivityType;
 import nl.limesco.cserv.sim.api.Sim;
 import nl.limesco.cserv.sim.api.SimService;
 
+import org.apache.commons.mail.EmailAttachment;
+import org.apache.commons.mail.MultiPartEmail;
 import org.osgi.service.log.LogService;
 
 import com.google.common.base.Optional;
-import com.google.common.base.Throwables;
 import com.google.common.collect.Sets;
 
 public class BatchInvoicingServiceImpl implements BatchInvoicingService {
 	
 	private final InvoiceConstructor invoiceConstructor;
+	
+	private volatile EmailService emailService;
 
 	private volatile AccountService accountService;
 	
@@ -82,6 +88,61 @@ public class BatchInvoicingServiceImpl implements BatchInvoicingService {
 					final Optional<? extends Account> account = accountService.getAccountById(accountId);
 					final byte[] pdf = invoiceTransformationService.transformToPdf(invoice, account.get());
 					writeToFile(invoice.getId() + ".pdf", pdf);
+					
+					final MultiPartEmail email = emailService.newMultiPartEmail();
+					email.addTo(account.get().getEmail(), account.get().getFullName().getFullName());
+					email.setFrom("mark@limesco.org", "Mark van Cuijk");
+					email.addReplyTo("support@limesco.nl", "Limesco support");
+					email.setSubject("Limesco factuur " + invoice.getId());
+					
+					final String formattedPrice = BigDecimal.valueOf(invoice.getTotalWithTaxes())
+							.divide(BigDecimal.valueOf(10000))
+							.setScale(2, RoundingMode.HALF_UP)
+							.toPlainString()
+							.replaceAll("\\.", ",");
+					final StringBuilder sb = new StringBuilder();
+					sb.append("Dag ").append(account.get().getFullName().getFirstName()).append(",\n\n")
+							.append("Sociale conventie schrijft voor dat het na de 6e van januari niet ")
+							.append("meer is toegestaan om nieuwjaarswensen uit te wisselen. ")
+							.append("Tegen deze conventie in wenst Limesco je het allerbeste toe ")
+							.append("in 2013!\n\n")
+							.append("Bijgevoegd tref je de laatste factuur van Limesco. ")
+							.append("Het totaalbedrag van EUR ").append(formattedPrice)
+							.append(" dienst te worden voldaan binnen 14 dagen op rekening ")
+							.append("1692.07.587 onder vermelding van factuurnummer ")
+							.append(invoice.getId()).append(".\n\n")
+							.append("Merk op dat gesprekken naar bestemmingen buiten Nederland ")
+							.append("en nummers met afwijkende tarieven nog niet in deze factuur zijn ")
+							.append("opgenomen. Deze gesprekken blijven wel in de database bewaard ")
+							.append("en worden in een toekomstige factuur meegenomen. ")
+							.append("Wij hebben gekozen voor deze aanpak, omdat we nog niet voldoende ")
+							.append("vertrouwen hebben dat de afhandeling van deze gesprekken ")
+							.append("foutloos verloopt en we willen niet dat jij daar de dupe van wordt. ")
+							.append("De bijdrage vaste kosten blijft nog ongewijzigd, omdat we nog ")
+							.append("niet de 150 gebruikers hebben bereikt.\n\n")
+							.append("Ondertussen zijn wij druk ")
+							.append("bezig met de bouw van een nieuwe website. Zodra deze af is ")
+							.append("en online staat, gaan we meer publiciteit zoeken om het ")
+							.append("aantal gebruikers te verhogen. Omdat de website van de hele community is ")
+							.append("zoeken we je hulp. Ben je tevreden met Limesco en vind je het ")
+							.append("belangrijk wat wij doen, stuur ons dan een korte tekst (bijv. 2 of 3 korte zinnen) met ")
+							.append("je ervaring van Limesco of waarom je jezelf bij de community ")
+							.append("hebt aangesloten. Wij zullen je daar erg dankbaar voor zijn!\n\n")
+							.append("Heb je vragen over deze factuur? Mail even naar support@limesco.nl ")
+							.append("en zorg dat je het factuurnummer ").append(invoice.getId())
+							.append(" vermeldt, zodat wij je snel kunnen helpen.\n\n")
+							.append("Met vriendelijke groet,\n")
+							.append("Mark van Cuijk\n\n");
+					email.setMsg(sb.toString());
+					
+					final EmailAttachment attachment = new EmailAttachment();
+					attachment.setPath(invoice.getId() + ".pdf");
+					attachment.setDisposition(EmailAttachment.ATTACHMENT);
+					attachment.setDescription("Limesco factuur " + invoice.getId());
+					attachment.setName(invoice.getId() + ".pdf");
+					email.attach(attachment);
+					
+					email.send();
 				}
 			} catch (IdAllocationException e) {
 				logService.log(LogService.LOG_WARNING, "Failed to allocate ID to invoice for " + accountId);
