3f3d9d9d2668b40bfae490b68a6fe4aa33c27444
Sjors Gielen <sjors@limesco.org>
2013-09-08 20:17:25 +0200

    Only give the DUMMY_DEACTIVATE proposed update if an account is not an external stub account.

--- nl.limesco.cserv.account/src/nl/limesco/cserv/account/api/AccountChecker.java
+++ nl.limesco.cserv.account/src/nl/limesco/cserv/account/api/AccountChecker.java
@@ -39,7 +39,7 @@ public class AccountChecker {
 	}
 	
 	public static enum ProposedChangeIdentifier {
-		DUMMY_DEACTIVATE("Dummy account, proposed deactivation"),
+		DUMMY_DEACTIVATE("Dummy account without external references, proposed deactivation"),
 		EXTERNAL_DUMMY_MERGE("Dummy account with external references, proposed merge with real account"),
 		NO_EXTERNAL_REF("Account has SIMs but no external SpeakUp reference, so can't be matched with CDRs"),
 		ASK_CONFIRMATION("Ask the user for confirmation"),
@@ -76,11 +76,12 @@ public class AccountChecker {
 	
 	// Checkers //
 	protected void checkDummyAccount() {
-		if(account.getEmail() == null || account.getEmail().isEmpty()
+		if(account.getState() != AccountState.EXTERNAL_STUB &&
+		  (account.getEmail() == null || account.getEmail().isEmpty()
 		|| account.getFullName() == null || account.getFullName().getFirstName() == null
 		|| account.getFullName().getFirstName().isEmpty()
 		|| account.getFullName().getLastName() == null
-		|| account.getFullName().getLastName().isEmpty()) {
+		|| account.getFullName().getLastName().isEmpty())) {
 			// It's not a real account, because essential properties are unset
 			ProposedChange c = new ProposedChange(ProposedChangeIdentifier.DUMMY_DEACTIVATE);
 			c.changes.put("state", AccountState.DEACTIVATED.toString());
